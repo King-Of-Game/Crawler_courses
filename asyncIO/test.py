d322b8a2bb6ca86b910d86971e285c3f2b6d9c1a
King-Of-Game <871437338@qq.com>
2020-10-08 17:09:46 +0800

    yixuan

--- asyncIO/test.py
+++ asyncIO/test.py
@@ -1,63 +1,34 @@
-# _*_ coding : utf-8 _*_
+# __coding__ : utf-8
 # __author__ : YiXuan
-# __date__ : 2019/10/15 10:11
+# __date__ : 9/12/2020 3:50 PM
 # __software__ : PyCharm
-# @Software: PyCharm
-
-import time
-import asyncio
 
 
-# 子生成器函数
-async def add():
-    await asyncio.sleep(2)
+# 消费者
+def consumer():
+    r = ''
+    while True:
+        n = yield r  # yield 后面的变量必须有值，而return后面可以什么都没有
+        if not n:  # 如果n不存在则执行
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
+# 生产者
+def produce(c):
+    c.send(None)  # 启动生成器
+    n = 0
+    while n < 3:
+        n = n + 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s' % r)
+    c.close()
 
-
-async def reduce():
-    await asyncio.sleep(3)
-
-
-
-# 协程（委托生成器）函数
-async def taskOne():
-    await add()
-
-async def taskMore():
-    tasks = [add(), reduce()]
-    done, pending = await asyncio.wait(tasks)
-    for i in done:
-        print('协程无序返回值：' + str(i.result()))
-
-
-
-async def test():
-    await asyncio.sleep(2)
-    print('*' * 20)
-
-async def hello():
-    print('hello1')
-    await asyncio.sleep(2)
-    print('hello2')
-
-
-def main():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(hello())
-    # tasks = [test(), hello()]  #
-    # loop.run_until_complete(asyncio.wait(tasks))  # 多个函数使用：asyncio.wait()
-    loop.close()
 
 
 if __name__ == '__main__':
-    start = time.time()
-    main()
-    print('总共用时：%.5f' % float(time.time() - start))
-
-
-
-
-
-
+    c = consumer()
+    produce(c)
 
 
 
